4b364dbc713b6d7f649ee71725bc72805563e5f5
Elio-ZWD <z704554363@gmail.com>
2026-08-04 20:30:11 +0800

    fix: 补充 Compose UI 实验 API 授权

--- app/src/main/java/com/elio/jianyu/ui/App.kt
+++ app/src/main/java/com/elio/jianyu/ui/App.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
@@ -77,7 +78,7 @@ fun MainAppContent(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 internal fun MainAppContent(
     viewModel: RoundtableViewModel,
