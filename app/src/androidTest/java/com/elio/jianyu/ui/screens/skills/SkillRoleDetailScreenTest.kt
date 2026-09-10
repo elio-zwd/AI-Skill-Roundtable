ecfa48d13c9ddeddaed565fab56df19500e8ab06
Elio-ZWD <z704554363@gmail.com>
2026-09-11 00:22:45 +0800

    test(ui-02): use supported detail assertions

--- app/src/androidTest/java/com/elio/jianyu/ui/screens/skills/SkillRoleDetailScreenTest.kt
+++ app/src/androidTest/java/com/elio/jianyu/ui/screens/skills/SkillRoleDetailScreenTest.kt
@@ -1,6 +1,5 @@
 package com.elio.jianyu.ui.screens.skills
 
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
